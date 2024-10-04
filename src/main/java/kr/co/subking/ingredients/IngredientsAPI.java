bf4d509738c04b96454b9fb630dc10ad16317961
LeeKangHo1 <kangho.lee.pr@gmail.com>
2024-10-04 17:47:30 +0900

    <temp> 재료 정보 받아오기 위해 준비 중

--- src/main/java/kr/co/subking/ingredients/IngredientsAPI.java
+++ src/main/java/kr/co/subking/ingredients/IngredientsAPI.java
@@ -13,12 +13,34 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.json.JsonMapper;
 
-@WebServlet("/api/v1/ingredients")
+@WebServlet({ "/api/v1/ingredients", "/api/v1/ingredients/*" })
 public class IngredientsAPI extends HttpServlet {
-	
+	private IngredientsService ingredientsService = IngredientsServiceImpl.getInstance();
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+
+	// custom_menu_id (버거 1개)의 재료 정보들을 가져오기 위해 설정
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String uri = req.getRequestURI();
+
+		Integer menu_id = parsingIdPath(uri);
 		
+		// 문제가 있다. custom_ig_id, custom_count인데 필요한건 이름이다.
+		// 새로운 객체를 만들어야 하나? custom_ig_id, ig_name, custom_count 3개를 필드로 가지는
+		// JOIN해서 select하고
+//		List<Ingredients> list = ingredientsService.getIgListByMenuId(menu_id);
+	}
+
+	private Integer parsingIdPath(String uri) {
+		int lastIndexOf = uri.lastIndexOf('/');
+		String idPath = uri.substring(lastIndexOf + 1);
+		Integer menu_id = Integer.valueOf(idPath);
+
+		return menu_id;
 	}
 
 }
--- src/main/java/kr/co/subking/ingredients/IngredientsMapper.java
+++ src/main/java/kr/co/subking/ingredients/IngredientsMapper.java
@@ -2,10 +2,12 @@ package kr.co.subking.ingredients;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 public interface IngredientsMapper {
 	
 	@Select("select ig_no, ig_name, ig_category, ig_price, ig_kcal from ingredients")
 	List<Ingredients> selectIg();
+	
 }
