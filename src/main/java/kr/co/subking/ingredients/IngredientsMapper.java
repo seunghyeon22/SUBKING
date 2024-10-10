c30ca0077906b6eff5a5c57162f6e51d3218b920
cfyle <cfyle@JY>
2024-10-10 09:33:48 +0900

    1

--- src/main/java/kr/co/subking/ingredients/IngredientsMapper.java
+++ src/main/java/kr/co/subking/ingredients/IngredientsMapper.java
@@ -9,7 +9,6 @@ public interface IngredientsMapper {
 
 	@Select("select ig_no, ig_name, ig_category, ig_price, ig_kcal from ingredients")
 	List<Ingredients> selectAll();
-
 	
 	@Select("SELECT I.ig_name, C.custom_count FROM custom AS C \n"
 			+ "	JOIN ingredients AS I ON C.custom_ig_id = I.ig_no AND custom_menu_id = #{custom_menu_id}")
